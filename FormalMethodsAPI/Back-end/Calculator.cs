f4c07c4303d9bef649d8d9ea2c6b29be2f386e96
tim <timdebooij9@hotmail.com>
2022-02-10 21:21:52 +0100

    [Added] Initial Project

---
+++ FormalMethodsAPI/Back-end/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormalMethodsAPI.Back_end
+{
+    public class Calculator
+    {
+
+        public string getString()
+        {
+            string r = "henlo, the number is: ";
+            return r + getNumbers(3, 8);
+        }
+
+        public int getNumbers(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+    }
+}
---
+++ FormalMethodsAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using FormalMethodsAPI.Back_end;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormalMethodsAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        public string Get()
+        {
+            {
+                var rng = new Random();
+                Calculator c = new Calculator();
+                return c.getString();
+                /*return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Summary = c.getString()
+                    //Date = DateTime.Now.AddDays(index),
+                    //TemperatureC = rng.Next(-20, 55),
+                    //Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();*/
+            }
+
+        }
+    }
+}
